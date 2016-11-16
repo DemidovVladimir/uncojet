0def1417c3915bcb5b09bacc5693574050fe0d0c
vladimir050486 <uncojet@gmail.com>
2016-11-16 15:36:14 +0600

    db

--- server.js
+++ server.js
@@ -117,6 +117,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-http.listen(80,'46.101.107.111/', function(){
+http.listen(80,'46.101.107.111', function(){
     console.log('listening on 80');
 });
