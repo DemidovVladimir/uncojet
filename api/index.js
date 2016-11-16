c53e9f9d3b3860c7e15b1f14d05922a17e29ba87
vladimir050486 <uncojet@gmail.com>
2016-11-16 17:01:55 +0600

    fixing db

--- api/index.js
+++ api/index.js
@@ -741,21 +741,17 @@ exports.postCategoryOutOfFile = function(req,res,next){
     if(req.body.videoLinks) videoLinks = req.body.videoLinks;
     if(req.body.order) order = req.body.order;
 
-
-    // db.categoryModel.find({}, function(err, data){
-      // if(err) console.log(err);
-      // data ? console.log(data) : null;
-      // res.send(data);
-        db.categoryModel.create({
-          cat_title:"title_mike",
-          cat_about:"about",
-          cat_areas:["areas"],
-          cat_order:"order"
-        },function(err){
-            if(err) return next(err);
-            res.send(200);
-        });
-    // })
+    db.categoryModel.update({
+      cat_title:title
+    },{
+      cat_about:about,
+      cat_areas:areas,
+      cat_videos:videoLinks,
+      cat_order:order
+    },{upsert:true},function(err){
+        if(err) return next(err);
+        res.send(200);
+    });
 }
 exports.getEquipmentsTotal = function(req,res,next){
     db.equipmentModel.aggregate({$sort:{equipment_order:1}},function(err,data){
