b842de5c2f5bcae166d457257aaa092eefa8f68a
vladimir050486 <uncojet@gmail.com>
2016-11-16 15:52:44 +0600

    fixing db

--- api/index.js
+++ api/index.js
@@ -731,27 +731,30 @@ exports.postAreaOutOfFile = function(req,res,next){
 //At the menu administration posting data without photo
 exports.postCategoryOutOfFile = function(req,res,next){
     var title = req.body.title;
-    var about = '';
-    var areas = [];
-    var videoLinks = [];
-    var order = '';
+    res.type('application/json');
+    res.status(404).send({'title': req.body.title});
 
-    if(req.body.about) about = req.body.about;
-    if(req.body.areas) areas = req.body.areas;
-    if(req.body.videoLinks) videoLinks = req.body.videoLinks;
-    if(req.body.order) order = req.body.order;
-    
-    db.categoryModel.update({
-      cat_title:title
-    },{
-      cat_about:about,
-      cat_areas:areas,
-      cat_videos:videoLinks,
-      cat_order:order
-    },{upsert:true},function(err){
-        if(err) return next(err);
-        res.send(200);
-    });
+    // var about = '';
+    // var areas = [];
+    // var videoLinks = [];
+    // var order = '';
+    //
+    // if(req.body.about) about = req.body.about;
+    // if(req.body.areas) areas = req.body.areas;
+    // if(req.body.videoLinks) videoLinks = req.body.videoLinks;
+    // if(req.body.order) order = req.body.order;
+    //
+    // db.categoryModel.update({
+    //   cat_title:title
+    // },{
+    //   cat_about:about,
+    //   cat_areas:areas,
+    //   cat_videos:videoLinks,
+    //   cat_order:order
+    // },{upsert:true},function(err){
+    //     if(err) return next(err);
+    //     res.send(200);
+    // });
 }
 exports.getEquipmentsTotal = function(req,res,next){
     db.equipmentModel.aggregate({$sort:{equipment_order:1}},function(err,data){
